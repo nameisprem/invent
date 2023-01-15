70bb28a2e4668d404b1b3291cc94fd45f0840e73
nameisprem <mpremkr09@gmail.com>
2023-01-16 04:10:43 +0530

    Added Middlewares & imps

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,16 @@ const cors = require("cors")
 
 const app = express()
 
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//Routes
+app.get("/", (req, res) => {
+    res.send("Home Page")
+});
+
 const PORT = process.env.PORT || 5000;
 
 //Connect to Database
