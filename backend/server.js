4aa807dc7fb6c48dd077f5b216c0d91088b472bc
nameisprem <mpremkr09@gmail.com>
2023-01-18 21:06:25 +0530

    Register user

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors")
-
+const userRoute = require("./routes/userRoute")
+const errorHandler = require('./middleware/errorMiddleware')
 
 const app = express()
 
@@ -12,15 +13,21 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+//Route Middleware
+app.use("/api/users", userRoute);
+
 //Routes
 app.get("/", (req, res) => {
     res.send("Home Page")
 });
 
+
+//Error middleware
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000;
 
 //Connect to Database
-
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
