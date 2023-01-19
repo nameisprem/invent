fab37437ef6bb4d61298d1f1cd48103e46475ff3
nameisprem <mpremkr09@gmail.com>
2023-01-19 22:47:15 +0530

    change password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -165,7 +165,7 @@ const loginStatus = asyncHandler(async (req, res) => {
 // Update User
 const updateUser = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id);
-
+    console.log(user)
     if (user) {
         const { name, email, photo, phone, bio } = user;
         user.email = email;
@@ -190,6 +190,33 @@ const updateUser = asyncHandler(async (req, res) => {
 });
 
 
+const changePassword = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    const { oldPassword, password } = req.body;
+
+    if (!user) {
+        res.status(400);
+        throw new Error("User not found, please signup");
+    }
+    //Validate
+    if (!oldPassword || !password) {
+        res.status(400);
+        throw new Error("Please add old and new password");
+    }
+
+    // check if old password matches password in DB
+    const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);
+
+    // Save new password
+    if (user && passwordIsCorrect) {
+        user.password = password;
+        await user.save();
+        res.status(200).send("Password change successful");
+    } else {
+        res.status(400);
+        throw new Error("Old password is incorrect");
+    }
+});
 
 
 module.exports = {
@@ -198,5 +225,6 @@ module.exports = {
     logoutUser,
     getUser,
     loginStatus,
-    updateUser
+    updateUser,
+    changePassword
 }
